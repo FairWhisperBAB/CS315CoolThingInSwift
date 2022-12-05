513b0c273b962e2e966eb923a96a3e564e34c29a
Benjamin Briggs <bbriggs2002@gmail.com>
2022-12-05 09:45:42 -0600

    Initial Commit

---
+++ CS315CoolThingInSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS315CoolThingInSwift
+//
+//  Created by Benjamin Briggs on 12/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
